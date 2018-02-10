79c6472d6813ba528a76f0edf048441d36d01109
slizbethellis <slizbethellis@gmail.com>
2018-02-10 02:25:00 -0600

    server.js and test view

---
+++ server.js
@@ -0,0 +1,135 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoTechnica database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoTechnica";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+
+// A GET route for scraping the Ars Technica website
+app.get("/scrape", function(req, res) {
+  axios.get("https://arstechnica.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+      result.summary = $(this).siblings(".excerpt").text();
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .findOne({ title: result.title })
+        .then(function(dbEntry) {
+          if (dbEntry === null) {
+            return db.Article
+              .create(result)
+              .then(function(dbArticle) {
+                // If we were able to successfully scrape and save an Article, send a message to the client
+                res.send("Scrape Complete");
+              })
+              .catch(function(err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+              });
+          }
+          else {
+            return res.send("Entry exists.");
+          }
+        });
+    });
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  db.Article
+    .find({})
+    .then(function(dbArticle) {
+      var hbObject = {
+        Articles: dbArticle
+      };
+      res.render("index", hbObject);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  db.Article
+    .findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      var hbObject = {
+        Articles: dbArticle
+      };
+      res.render("index", hbObject);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      var hbObject = {
+        Articles: dbArticle
+      };
+      res.render("index", hbObject);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
