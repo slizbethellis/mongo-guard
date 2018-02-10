1679a8b9787348628d525bcafa2b18a30e23107f
slizbethellis <slizbethellis@gmail.com>
2018-02-10 00:54:43 -0600

    completed models

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+  // `link` is required and of type String
+  link: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: false
+  },
+  // `note` is an associate object
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+// exports an object containing all models
+module.exports = {
+  Article: require("./Article"),
+  Note: require("./Note")
+};
