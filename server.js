79daf7b0c1ad86bfb2a38301525164e3cad60983
slizbethellis <slizbethellis@gmail.com>
2018-04-04 07:24:51 -0500

    what are you doing, heroku

--- server.js
+++ server.js
@@ -24,12 +24,6 @@ mongoose.connect(MONGODB_URI, {
   //useMongoClient: true
 });
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to Mongoose!')
-});
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
